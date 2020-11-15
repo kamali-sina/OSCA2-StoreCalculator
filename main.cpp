0c76d7d85d0260268f068095f9bcd3926fce7c9c
Sina Kamali <sininoir@gmail.com>
2020-11-16 00:35:18 +0330

    added file saving to main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
-#include <stdlib.h>
-#include <string>
-#include <string.h>
-#include <iostream>
-#include <vector>
-#include <fstream>
-
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h> 
+#include "parser.hpp"
+#include "forker.hpp"
 
 using namespace std;
 
@@ -21,37 +12,15 @@ int main(int argc, char* argv[]){
     string commands_path = argv[1];
     string asset_dir = argv[2];
     fstream yourfile;
+    ofstream ofile;
+    ofile.open("./result.out");
     yourfile.open(commands_path, ios::in | ios::app);
-    while (getline(yourfile,line)){
-        cout<<line<<endl;
-        //fork main thing here with line and asset_dir
+    while(getline(yourfile,line)){
+        int result = fork_main(line, asset_dir);
+        cout<<"result was: "<<result<<endl;
+        ofile<<result<<endl;
     }
+    ofile.close();
+    yourfile.close();
     return 0;
 }
-
-/* 
-unnamed pipe:
-
-int pipefd[2];
-pipe(pipefd);
-write(pipefd[1], "kossssss",strlen("kossssss") + 1);
-char baboole[100];
-read(pipefd[0], &baboole, strlen("kossssss") + 1);
-*/
-
-/*
-named pipe:
-
-char buf[100];
-int fd; 
-char myfifo[] = "./myfifo"; 
-mkfifo(myfifo, 0666);
-fd = open(myfifo, O_WRONLY);
-write(fd, "ya ya ya", strlen("ya ya ya") + 1); 
-cout<<"wrote shit"<<endl;
-close(fd);
-fd = open(myfifo, O_RDONLY); 
-read(fd, buf, sizeof("ya ya ya") + 1);
-cout<<"read shit"<<endl;
-cout << "read: "<<buf<<endl;
-*/
